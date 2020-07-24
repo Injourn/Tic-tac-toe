5c4f95374c78a4ecf496f4261eff13d48b0b94b4
Scott-GH <thatguywhowas@live.com>
2020-07-23 17:11:53 -0700

    Added basic components to the game

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    //Props are the properties or attributes <[tag] [attribute]=[attributeValue] />
+    constructor(props){
+        super(props);
+        this.state = {
+            //States act as the class values for a React component
+            value: null,
+        };
+    }
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button 
+            className="square"
+            onClick={() => {this.setState({value:'X'})}}
+        >
+            {this.state.value}
         </button>
       );
     }
@@ -14,7 +25,7 @@ class Square extends React.Component {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
